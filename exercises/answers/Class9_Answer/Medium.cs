17a8586805449d11ecb5549c196833e4d7286b4c
giginepuran <61973060+giginepuran@users.noreply.github.com>
2021-08-10 01:17:41 +0800

    Update Medium.cs

--- exercises/answers/Class9_Answer/Medium.cs
+++ exercises/answers/Class9_Answer/Medium.cs
@@ -1,45 +1,38 @@
 using System;
-namespace Class9
+namespace class9
 {
     class Medium
     {
         public static void Run()
         {
             string equation = Console.ReadLine();
-            if (equation.Contains('+'))
+            char separator;
+            if (equation.Contains('+')) { separator = '+'; }
+            else if (equation.Contains('-')) { separator = '-'; }
+            else if (equation.Contains('*')) { separator = '*'; }
+            else if (equation.Contains('/')) { separator = '/'; }
+            else {return;}
+            string[] nums = equation.Split(separator);
+            int num0, num1;
+            string result = "";
+            num0 = int.TryParse(nums[0], out num0)?int.Parse(nums[0]):Easy.Roman2Arabic(nums[0]);
+            num1 = int.TryParse(nums[1], out num1)?int.Parse(nums[1]):Easy.Roman2Arabic(nums[1]);
+            switch (separator)
             {
-                string[] nums = equation.Split('+');
-                int num0, num1;
-                num0 = int.TryParse(nums[0], out num0)?int.Parse(nums[0]):Easy.Roman2Arabic(nums[0]);
-                num1 = int.TryParse(nums[1], out num1)?int.Parse(nums[1]):Easy.Roman2Arabic(nums[1]);
-                Console.WriteLine(Easy.Arabic2Roman(num0+num1)); 
+                case '+':
+                result = Easy.Arabic2Roman(num0 + num1);
+                break;
+                case '-':
+                result = Easy.Arabic2Roman(num0 - num1);
+                break;
+                case '*':
+                result = Easy.Arabic2Roman(num0 * num1);
+                break;
+                case '/':
+                result = Easy.Arabic2Roman(num0 / num1);
+                break;
             }
-            else if (equation.Contains('-'))
-            {
-                string[] nums = equation.Split('-');
-                int num0, num1;
-                num0 = int.TryParse(nums[0], out num0)?int.Parse(nums[0]):Easy.Roman2Arabic(nums[0]);
-                num1 = int.TryParse(nums[1], out num1)?int.Parse(nums[1]):Easy.Roman2Arabic(nums[1]);
-                string result = Easy.Arabic2Roman(num0-num1);
-                Console.WriteLine(result==""?"zero":result);
-            }
-            else if (equation.Contains('*'))
-            {
-                string[] nums = equation.Split('*');
-                int num0, num1;
-                num0 = int.TryParse(nums[0], out num0)?int.Parse(nums[0]):Easy.Roman2Arabic(nums[0]);
-                num1 = int.TryParse(nums[1], out num1)?int.Parse(nums[1]):Easy.Roman2Arabic(nums[1]);
-                Console.WriteLine(Easy.Arabic2Roman(num0*num1)); 
-            }
-            else if (equation.Contains('/'))
-            {
-                string[] nums = equation.Split('/');
-                int num0, num1;
-                num0 = int.TryParse(nums[0], out num0)?int.Parse(nums[0]):Easy.Roman2Arabic(nums[0]);
-                num1 = int.TryParse(nums[1], out num1)?int.Parse(nums[1]):Easy.Roman2Arabic(nums[1]);
-                string result = Easy.Arabic2Roman(num0/num1);
-                Console.WriteLine(result==""?"zero":result);
-            }
+            Console.WriteLine(result==""?"zero":result);
         }
     }
 
